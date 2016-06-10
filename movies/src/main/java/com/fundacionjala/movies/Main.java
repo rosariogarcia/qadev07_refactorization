d5221060a085c0b752ab17265c5c4cc4a74b790f
Rosario Garcia Ocampo <RosarioGarcia@Fundacion-jala.local>
2016-06-09 20:24:50 -0400

    update files

---
+++ movies/src/main/java/com/fundacionjala/movies/Main.java
@@ -0,0 +1,10 @@
+package com.fundacionjala.movies;
+
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Test");
+        customer.addRental(new Rental(new Movie("The Revenant", 1), -2));
+        customer.addRental(new Rental(new Movie("Terminator", 0), 3));
+        System.out.println(customer.statement());
+    }
+}
